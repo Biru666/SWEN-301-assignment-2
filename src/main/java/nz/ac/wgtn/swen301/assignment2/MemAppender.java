03d418d48df71c20ca38ee8bdac488744966042c
Biru666 <68627484+Biru666@users.noreply.github.com>
2021-04-28 23:28:30 +1200

    untested version of MemAppender

--- src/main/java/nz/ac/wgtn/swen301/assignment2/MemAppender.java
+++ src/main/java/nz/ac/wgtn/swen301/assignment2/MemAppender.java
@@ -3,27 +3,48 @@ package nz.ac.wgtn.swen301.assignment2;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Layout;
 import org.apache.log4j.spi.LoggingEvent;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import sun.rmi.runtime.Log;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class MemAppender extends AppenderSkeleton {
-    private final List<LoggingEvent> Logs = new LinkedList<LoggingEvent>();
+    private List<LoggingEvent> logs = new LinkedList<>();
     private long maxSize = 1000;
-    private final long discardedLogCount = 0;
+    private long discardedLogCount = 0;
 
 
     public MemAppender(){}
 
-    public MemAppender(Layout layout , String filename) throws IOException {
+    public MemAppender(Layout layout) throws IOException {
         this.layout = layout;
-        this.name = filename;
+    }
+
+    public void exportToJSON(String fileName) throws IOException{
+        FileWriter file = new FileWriter(fileName);
+        JSONLayout layout = new JSONLayout();
+        JSONArray jsonArr = new JSONArray();
+        for (LoggingEvent event: logs){
+            JSONObject obj = new JSONObject(layout.format(event));
+            jsonArr.put(obj);
+        }
+        file.write(jsonArr.toString());
+        file.flush();
     }
 
     @Override
     protected void append(LoggingEvent loggingEvent) {
-
+        if (logs.size() >= maxSize) {
+            logs.remove(0);
+            discardedLogCount++;
+        }
+        this.logs.add(loggingEvent);
     }
 
     @Override
@@ -37,7 +58,7 @@ public class MemAppender extends AppenderSkeleton {
     }
 
     public List<LoggingEvent> getCurrentLogs() {
-        return Logs;
+        return Collections.unmodifiableList(logs);
     }
 
     public long getDiscardedLogCount() {
